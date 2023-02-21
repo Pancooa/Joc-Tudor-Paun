371d26d55870bfc66e0b1254c9ac93db1fadcd1a
Pancooa <tudorunderline@gmail.com>
2023-02-21 18:21:24 +0200

    adaugat scara, eu serban tudor si nu paun liviu stefan

---
+++ Joc tp/Assets/nivelobstacole/scara.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scara : MonoBehaviour
+{
+    public bool conexiune;
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer==11)
+        {
+            conexiune = false;
+
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        if (collision.gameObject.layer==11)
+        {
+            conexiune = true;
+
+        }
+         
+        
+    }
+
+    
+
+        
+    
+
+    void Start()
+
+    { }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Joc tp/Assets/player/urcaPeScara.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urcaPeScara : MonoBehaviour
+{
+    public scara urcascara;
+    public Rigidbody2D corp;
+    public float miscare;
+    public bool test;
+    public bool apasE;
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) == true)
+        {
+            apasE = true;
+        }
+
+        if (apasE==true & urcascara.conexiune==true)
+
+        {
+            corp.velocity = new Vector2(0,0);
+            test = true;
+
+        }
+
+
+        if (Input.GetKey(KeyCode.E ) & urcascara.conexiune==true)
+        {
+            apasE = false;
+            corp.velocity = new Vector2(0, miscare);
+
+        }
+
+        if (Input.GetKey(KeyCode.R) & urcascara.conexiune == true)
+        
+            corp.velocity = new Vector2(0, -miscare);
+        }
+
+}
