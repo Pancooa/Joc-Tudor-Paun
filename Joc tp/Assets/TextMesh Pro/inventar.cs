bd43398102ff03830bbf3abe617441b9fb4d94ed
Pancooa <tudorunderline@gmail.com>
2023-02-25 02:21:28 +0200

    Revert "Update"
    This reverts commit 37b87e19e36ba4e29975fdd72f9c84a0862f2c94.

---
+++ Joc tp/Assets/TextMesh Pro/inventar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class inventar : MonoBehaviour
+{
+
+
+    public int Value = 10;
+    public Text  ValueText;
+
+
+    void Start()
+    {
+        
+
+
+
+    }
+
+    
+    void Update()
+    {
+
+
+        ValueText.text = Value.ToString();
+
+    }
+}
