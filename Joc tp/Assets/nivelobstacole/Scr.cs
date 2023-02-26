6accc98221545f0175b90e751d8c17bb63daa19b
ZealotCell <74587690+QFZealot@users.noreply.github.com>
2023-02-26 23:02:30 +0200

    Cheie

---
+++ Joc tp/Assets/nivelobstacole/Scr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr : MonoBehaviour
+{
+    public bool PlayerEaCheie;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.gameObject.layer == 11)
+        {
+            PlayerEaCheie = true;
+
+            Destroy(gameObject);
+
+
+        }
+    }
+
+}
