85c517aecb9a722a78d4c5431c98539dd555c849
ZealotCell <74587690+QFZealot@users.noreply.github.com>
2023-03-01 20:21:56 +0200

    MiniMap-Si-Inventar

---
+++ Joc tp/Assets/nivelobstacole/KEYE/Ridicare.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ridicare : MonoBehaviour {
+
+    public INVENTAR inventory;
+    public GameObject itemButton; 
+
+
+
+private void start()
+
+{
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<INVENTAR>();
+
+}
+    
+void onTriggerEnter2d(Collider2D other)
+{
+
+    if (other.CompareTag("Player"))
+    {
+                             
+        for ( int i = 0; i < inventory.slots.Length; i++)
+        {
+            if  (inventory.isFull[i] == false)
+            {
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+            }
+        }
+    }
+
+}
+
+
+
+    
+
+     
+
+}
